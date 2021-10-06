5fbf4d4449c75edfb37d39707ed0e2d91da949c8
automorphis <mlanetheta@gmail.com>
2021-10-06 13:45:45 -0400

    licensing

--- test/basic/test_polynomials.py
+++ test/basic/test_polynomials.py
@@ -1,3 +1,18 @@
+"""
+    Beta Expansions of Salem Numbers, calculating periods thereof
+    Copyright (C) 2021 Michael P. Lane
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+"""
+
 import copy
 from itertools import chain, product
 from unittest import TestCase
