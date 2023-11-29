26551691edc5543fcfd4251ddf7b750decd641cb
automorphis <mlanetheta@gmail.com>
2023-11-29 16:18:56 -0500

    updates

--- scripts/temp.py
+++ scripts/temp.py
@@ -5,12 +5,12 @@ from intpolynomials.registers import IntPolynomialRegister
 perron_polys_reg = load_shorthand('perron_polys_reg', '/fs/project/thompson.2455/lane.662/perronnums')
 perron_nums_reg = load_shorthand('perron_nums_reg', '/fs/project/thompson.2455/lane.662/perronnums')
 apri_exceptions = (ApriInfo(deg = 2, sum_abs_coef = 2), ApriInfo(deg = 3, sum_abs_coef = 2))
+dps = 500
 
-with openregs(perron_polys_reg, perron_nums_reg):
+with openregs(perron_polys_reg, perron_nums_reg, readonlys = (True, True)):
 
     for apri in perron_polys_reg:
-
-        assert apri in perron_nums_reg or apri in apri_exceptions
+        assert ApriInfo(deg = apri.deg, sum_abs_coef = apri.sum_abs_coef, dps = dps) in perron_nums_reg or apri in apri_exceptions
 
         if apri not in apri_exceptions:
             assert list(perron_nums_reg.intervals(apri)) == list(perron_polys_reg.intervals(apri))
@@ -20,5 +20,8 @@ with openregs(perron_polys_reg, perron_nums_reg):
             assert perron_polys_reg.is_compressed(apri, startn, length)
             assert perron_nums_reg.is_compressed(apri, startn, length)
 
+        apos = perron_polys_reg.apos(apri)
+        assert apos.complete or hasattr(apos, 'last_poly')
+
     for apri in perron_nums_reg:
-        assert apri in perron_polys_reg
+        assert ApriInfo(deg = apri.deg, sum_abs_coef = apri.sum_abs_coef) in perron_polys_reg
