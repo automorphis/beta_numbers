187c0508d0f4e0188d6ea32d3f1f694fb09f116a
automorphis <mlanetheta@gmail.com>
2023-11-29 10:53:34 -0500

    updates

--- scripts/temp.py
+++ scripts/temp.py
@@ -1,24 +1,20 @@
-from cornifer import load_shorthand, NumpyRegister, openregs
+from cornifer import load_shorthand, NumpyRegister, openregs, ApriInfo
 from beta_numbers.registers import MPFRegister
 from intpolynomials.registers import IntPolynomialRegister
 
 perron_polys_reg = load_shorthand('perron_polys_reg', '/fs/project/thompson.2455/lane.662/perronnums')
 perron_nums_reg = load_shorthand('perron_nums_reg', '/fs/project/thompson.2455/lane.662/perronnums')
+apri_exceptions = (ApriInfo(deg = 2, sum_abs_coef = 2), ApriInfo(deg = 3, sum_abs_coef = 2))
 
 with openregs(perron_polys_reg, perron_nums_reg):
 
     for apri in perron_polys_reg:
 
-        try:
-            assert apri in perron_nums_reg
+        assert apri in perron_nums_reg or apri in apri_exceptions
 
-        except AssertionError:
-            print(apri)
+        if apri not in apri_exceptions:
+            assert list(perron_nums_reg.intervals(apri)) == list(perron_polys_reg.intervals(apri))
+
 
     for apri in perron_nums_reg:
-
-        try:
-            assert apri in perron_polys_reg
-
-        except AssertionError:
-            assert apri in perron_nums_reg
+        assert apri in perron_polys_reg
