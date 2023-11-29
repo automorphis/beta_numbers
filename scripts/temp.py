c6c5d0d790e6e12edfadca701eca810f4e421fde
automorphis <mlanetheta@gmail.com>
2023-11-29 10:43:24 -0500

    updates

--- scripts/temp.py
+++ scripts/temp.py
@@ -2,8 +2,8 @@ from cornifer import load_shorthand, NumpyRegister, openregs
 from beta_numbers.registers import MPFRegister
 from intpolynomials.registers import IntPolynomialRegister
 
-perron_polys_reg = load_shorthand('perron_polys_reg', '/fs/project/thompson.2455/lane.662/betaorbits')
-perron_nums_reg = load_shorthand('perron_nums_reg', '/fs/project/thompson.2455/lane.662/betaorbits')
+perron_polys_reg = load_shorthand('perron_polys_reg', '/fs/project/thompson.2455/lane.662/perronnums')
+perron_nums_reg = load_shorthand('perron_nums_reg', '/fs/project/thompson.2455/lane.662/perronnums')
 
 with openregs(perron_polys_reg, perron_nums_reg):
 
