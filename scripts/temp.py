72883133bae89efaf297323f501d335155fd651a
automorphis <mlanetheta@gmail.com>
2023-11-29 16:34:49 -0500

    updates

--- scripts/temp.py
+++ scripts/temp.py
@@ -15,25 +15,19 @@ with openregs(perron_polys_reg, perron_nums_reg, readonlys = (False, False)):
         #     perron_polys_reg.change_apri(apri, ApriInfo(deg = apri.deg, sum_abs_coef = apri.sum_abs_coef))
 
         assert not hasattr(apri, 'dps')
+        nums_apri = ApriInfo(deg = apri.deg, sum_abs_coef = apri.sum_abs_coef, dps = dps)
+        assert nums_apri in perron_nums_reg or apri in apri_exceptions
 
-        try:
-            assert ApriInfo(deg = apri.deg, sum_abs_coef = apri.sum_abs_coef, dps = dps) in perron_nums_reg or apri in apri_exceptions
+        if apri not in apri_exceptions:
+            assert list(perron_nums_reg.intervals(nums_apri)) == list(perron_polys_reg.intervals(apri))
 
-        except AssertionError:
-            print(apri)
+        for startn, length in perron_polys_reg.intervals(apri):
 
-        else:
+            assert perron_polys_reg.is_compressed(apri, startn, length)
+            assert perron_nums_reg.is_compressed(nums_apri, startn, length)
 
-            if apri not in apri_exceptions:
-                assert list(perron_nums_reg.intervals(apri)) == list(perron_polys_reg.intervals(apri))
-
-            for startn, length in perron_polys_reg.intervals(apri):
-
-                assert perron_polys_reg.is_compressed(apri, startn, length)
-                assert perron_nums_reg.is_compressed(apri, startn, length)
-
-            apos = perron_polys_reg.apos(apri)
-            assert apos.complete or hasattr(apos, 'last_poly')
+        apos = perron_polys_reg.apos(apri)
+        assert apos.complete or hasattr(apos, 'last_poly')
 
     for apri in perron_nums_reg:
         assert ApriInfo(deg = apri.deg, sum_abs_coef = apri.sum_abs_coef) in perron_polys_reg
