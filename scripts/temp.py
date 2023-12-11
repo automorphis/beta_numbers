5b89fa636526bbdb8a61e9d38172b8191eda176f
automorphis <mlanetheta@gmail.com>
2023-12-10 19:37:19 -0500

    updates

--- scripts/temp.py
+++ scripts/temp.py
@@ -39,7 +39,10 @@ with stack(status_reg.open(True), periodic_reg.open(True), coef_orbit_reg.open(T
                     assert coef_orbit_reg.total_len(orbit_apri) == m + p + 1
                 except AssertionError:
                     print('hello', poly_orbit_reg.total_len(orbit_apri), m, p)
-                assert np.all(status_reg[poly_apri, index] == np.array([m + p, -1, -1]))
+                try:
+                    assert np.all(status_reg[poly_apri, index] == np.array([m + p, -1, -1]))
+                except AssertionError:
+                    print('hey', status_reg[poly_apri, index])
 
             else:
 
